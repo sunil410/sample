34599e3f2f4e35f35593e0c9fc3cb918de8c35a5
sunil410 <sunil.sahani@rakuten.com>
2022-05-24 15:32:43 +0530

    trying local file to gitt

--- Sunil.java
+++ Sunil.java
@@ -2,7 +2,7 @@ class Sunil{
   public static void main(String[] args){
     
 
-	System.out.println("Hello world Check Changes from local files");
+	System.out.println("Hello world i have done changes from remoly !!!");
 }
 
 }
